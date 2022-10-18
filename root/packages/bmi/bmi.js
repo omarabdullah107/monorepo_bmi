bb9ff7291af54d5f0c623a00ac24912e447a6b74
omarabdullah107 <mromar502@hotmail.com>
2022-10-18 21:59:32 +0200

    pushing updates

--- root/packages/bmi/bmi.js
+++ root/packages/bmi/bmi.js
@@ -1,4 +1,4 @@
 const convercmTom = require("../convert/convert");
-const bmi = (weight,height) => {return (weight/convercmTom(Math.pow(height,2)))*100}
+const bmi = (weight,height) => (weight/convercmTom(Math.pow(height,2)))*100
 // the bmi function divides the weight over the power of the height, converting the result of the height sqaured into meter.
 module.exports = bmi;
--- root/packages/convert/convert.js
+++ root/packages/convert/convert.js
@@ -1,2 +1,2 @@
-const convercmTom = (number) => {return number/100}
+const convercmTom = (number) => number/100
 module.exports = convercmTom; 
